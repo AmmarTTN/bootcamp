7afd921d61fd0c7b46e6e6ef57a4a2880bb74b4f
AmmarTTN <ammar.alam@tothenew.com>
2020-02-13 17:44:59 +0530

    javascript assignment

--- calculate.js
+++ calculate.js
@@ -47,3 +47,64 @@ function q4() {
   console.log("person2(copied object)", person2);
   document.querySelector("#obj").innerHTML = " please check console";
 }
+var employees = [
+  {
+    name: "ammar",
+    age: 22,
+    salary: 2000,
+    dob: "24/05/1997"
+  },
+  {
+    name: "anuj",
+    age: 23,
+    salary: 10000,
+    dob: "08/10/1996"
+  },
+  {
+    name: "ayush",
+    age: 22,
+    salary: 900,
+    dob: "24/1/1998"
+  },
+  {
+    name: "abdullah",
+    age: 23,
+    salary: 2000,
+    dob: "01/02/1997"
+  }
+];
+function view() {
+  document.querySelector("#employees").innerHTML = "please check console";
+  console.log(JSON.stringify(employees));
+}
+function checksalary() {
+  var result = document.querySelector("#salary");
+  var data = [];
+  data = employees.filter(function(item) {
+    return item.salary > 5000;
+  });
+  for (var i = 0; i < data.length; i++) {
+    if (result.innerHTML == "") result.innerHTML = data[i].name;
+    else result.innerHTML = result.innerHTML + "," + data[i].name;
+  }
+}
+function group() {
+  result = employees.reduce(function(r, a) {
+    r[a.age] = r[a.age] || [];
+    r[a.age].push(a);
+    return r;
+  }, Object.create(null));
+  console.log("employees group by age:", result);
+  document.querySelector("#age").innerHTML = "please check console";
+}
+function fetch() {
+  var data = [];
+  data = employees.filter(function(item) {
+    return item.salary < 1000 && item.age > 20;
+  });
+  data.forEach(function(item) {
+    item.salary = item.salary + item.salary * 5;
+  });
+  console.log("employees updated salary:", data);
+  document.querySelector("#fetch").innerHTML = "please check the console";
+}
