1ece6f753a577bb11e0bc9c8d0ef35a49ecd767a
AmmarTTN <ammar.alam@tothenew.com>
2020-02-18 00:50:34 +0530

    deleted and modified

--- src/ans11.java
+++ src/ans11.java
@@ -1,42 +1,56 @@
 //Q11.Create 3 sub class of bank SBI,BOI,ICICI all 4 should have method called getDetails
 // which provide there specific details like rateofinterest etc,print details of every banks
 public class ans11 {
+    float rateofinterest;
+    int accounts;
+    int branches;
     public static void main(String[] args) {
-        boi b=new boi();
+        boi b=new boi(5.0f,1000,20);
         System.out.println("details of BOI :-");
         b.getDetails();
-        icici i=new icici();
+        icici i=new icici(4.2f,1050,25);
         System.out.println("details of ICICI :-");
         i.getDetails();
-        sbi s=new sbi();
+        sbi s=new sbi(4.8f,2000,30);
         System.out.println("details of SBI :-");
         s.getDetails();
     }
 }
-class boi{
-    float rateofinterest=5.0f;
-    int accounts=1000;
-    int branches=20;
+class boi extends ans11{
+    boi(float f,int acc,int br)
+
+    {
+         rateofinterest =f;
+         accounts = acc;
+         branches = br;
+    }
     void getDetails(){
         System.out.println("No. of accounts: "+" "+accounts);
         System.out.println("Rate of interset: "+" "+rateofinterest);
         System.out.println("branches: "+" "+branches);
     }
 }
-class icici{
-    float rateofinterest=4.2f;
-    int accounts=1050;
-    int branches=25;
+class icici extends ans11{
+    icici(float f,int acc,int br)
+    {
+         rateofinterest =f;
+         accounts = acc;
+         branches = br;
+    }
     void getDetails(){
         System.out.println("No. of accounts: "+" "+accounts);
         System.out.println("Rate of interset: "+" "+rateofinterest);
         System.out.println("branches: "+" "+branches);
     }
 }
-class sbi{
-    float rateofinterest=5.0f;
-    int accounts=1000;
-    int branches=20;
+class sbi extends ans11{
+    sbi(float f,int acc,int br)
+
+    {
+        rateofinterest =f;
+        accounts = acc;
+        branches = br;
+    }
     void getDetails(){
         System.out.println("No. of accounts: "+" "+accounts);
         System.out.println("Rate of interset: "+" "+rateofinterest);
