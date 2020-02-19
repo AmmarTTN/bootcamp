dfb1d0d0510db5af5325c8bd592f96a05d5aae8b
AmmarTTN <ammar.alam@tothenew.com>
2020-02-20 00:53:54 +0530

    assignment

---
+++ ans5/Student.java
@@ -0,0 +1,42 @@
+package ans5;
+// b. Program to show object cloning using copy constructor
+
+public class Student{
+    private int studentId;
+    private String studentName;
+    private String studentDept;
+
+    public Student(int id, String name, String dept){
+        studentId = id;
+        studentName = name;
+        studentDept = dept;
+    }
+
+    public Student(Student student){
+        this.studentId = student.studentId;
+        this.studentName = student.studentName;
+        this.studentDept = student.studentDept;
+    }
+
+    public void showDetails(){
+        System.out.println("ID: " + studentId);
+        System.out.println("Name: " + studentName);
+        System.out.println("Department: " + studentDept + "\n");
+    }
+
+}
+
+class StudentDriver {
+    public static void main(String[] args) {
+//        Original student object
+        Student student = new Student(1, "ammar", "CSE");
+        System.out.println("Original student object:");
+        student.showDetails();
+
+//        Cloned object
+        Student clone = new Student(student);
+        System.out.println("Cloned Object:");
+        clone.showDetails();
+
+    }
+}
