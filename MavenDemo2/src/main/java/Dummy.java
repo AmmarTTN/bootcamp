db5ac26fbe6cff51c4982442e1153c27e2ba637b
AmmarTTN <ammar.alam@tothenew.com>
2020-03-03 01:47:12 +0530

    removed

--- MavenDemo2/src/main/java/Dummy.java
+++
@@ -1,9 +0,0 @@
-import org.apache.log4j.Logger;
-import org.apache.log4j.BasicConfigurator;
-public class Dummy {
-    public static void main(String[] args) {
-      BasicConfigurator.configure();
-        Logger l=Logger.getLogger(Dummy.class.getName());
-        l.info("hello");
-    }
-}
